3d4206a7a231808b7b794ca20109ebe90fc1bfaf
daton <rapidclimate@gmail.com>
2018-01-17 17:07:40 -0600

    Clave de alumno

--- src/main/java/org/geducativo/geradmin3/ControladorAutenticacion.java
+++ src/main/java/org/geducativo/geradmin3/ControladorAutenticacion.java
@@ -1,6 +1,7 @@
 package org.geducativo.geradmin3;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,11 @@ public class ControladorAutenticacion {
     @RequestMapping(value="/autenticar/profesor")
     public Estatus autenticarProfesor(@RequestBody String json)throws Exception{
         Estatus e=new Estatus();
+        ObjectMapper maper=new ObjectMapper();
+
+        Profesor profesor=maper.readValue(json, Profesor.class);
+        e.setMensaje("Ah llegado  ese profe");
+        System.out.println("Ha llegado el profesor " +json);
 
         return e;
     }
@@ -31,6 +37,11 @@ public class ControladorAutenticacion {
     @RequestMapping(value="/autenticar/alumno")
     public Estatus autenticarAlumno(@RequestBody String json)throws Exception{
         Estatus e=new Estatus();
+        ObjectMapper maper=new ObjectMapper();
+
+        Profesor profesor=maper.readValue(json, Profesor.class);
+        e.setMensaje("Ah llegado alumno burro");
+        System.out.println("Ha llegado el alumno "+json);
 
         return e;
     }
